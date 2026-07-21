2c5654ab9f97793334a8a01c7e65e3f168868fa5
塚田美幸 <tsukadamiyuki@tsukadamiyukinoMacBook-Air.local>
2026-07-21 22:06:02 +0900

    First Himari Bot

---
+++ himari.py
@@ -0,0 +1,38 @@
+import discord
+import os
+import json
+import random
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("HIMARI_TOKEN")
+
+with open("reply_list_himari.json", encoding="utf-8") as f:
+    REPLIES = json.load(f)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"{client.user} が起きました🐶")
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+       return
+    
+    if message.channel.name != "talk-to-himari":
+       return
+    
+    await message.channel.send(random.choice(REPLIES))
+
+client.run(TOKEN)
+
+
